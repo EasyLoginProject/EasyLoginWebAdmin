2e619a3438de40ada70044c2717804c004c9f875
Yoann Gini <yoann.gini@gmail.com>
2018-01-02 23:43:19 +0100

    Server URL relative to app URL

--- src/admin/src/App.js
+++ src/admin/src/App.js
@@ -7,7 +7,7 @@ import { UserList, UserEdit, UserCreate } from './Users';
 import UserIcon from 'material-ui/svg-icons/social/person';
 
 const App = () => (
-  <Admin title="EasyLogin" restClient={jsonServerRestClient('http://127.0.0.1:8080/admapi')}>
+  <Admin title="EasyLogin" restClient={jsonServerRestClient('/admapi')}>
       <Resource name="users" list={UserList} edit={UserEdit} create={UserCreate} remove={Delete} icon={UserIcon} />
   </Admin>
 );
