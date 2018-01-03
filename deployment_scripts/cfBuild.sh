2bcb77178f83d2e2e6da680ccda6f6ada9752a94
Yoann Gini <yoann.gini@gmail.com>
2018-01-03 17:43:35 +0100

    Improved deployment script

--- deployment_scripts/cfBuild.sh
+++ deployment_scripts/cfBuild.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
+if [ -d htdocs/admin ]
+then
+	rm -rf htdocs/admin
+fi
+
 cd src
 npm install
 npm run build
+returnCode=$?
+cd ..
 
-exit 0
+if [ $returnCode -eq 0 ]
+then
+	mv src/build htdocs/admin
+fi
+
+exit $returnCode
