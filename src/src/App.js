1a6d4890920a2fa0fa04673aa88259eb93ef8b29
Yoann Gini <yoann.gini@gmail.com>
2018-01-03 17:40:41 +0100

    Fixing REST client used, bad copy/paste Commenting base code for JWT since usless

--- src/src/App.js
+++ src/src/App.js
@@ -6,17 +6,17 @@ import { UserList, UserShow, UserEdit, UserCreate } from './Users';
 
 import UserIcon from 'material-ui/svg-icons/social/person';
 
-const httpClient = (url, options = {}) => {
-  if (!options.headers) {
-      options.headers = new Headers({ Accept: 'application/json' });
-  }
-  const token = localStorage.getItem('token');
-  options.headers.set('Authorization', `Bearer ${token}`);
-  return fetchUtils.fetchJson(url, options);
-}
+// const httpClient = (url, options = {}) => {
+//   if (!options.headers) {
+//       options.headers = new Headers({ Accept: 'application/json' });
+//   }
+//   const token = localStorage.getItem('token');
+//   options.headers.set('Authorization', `Bearer ${token}`);
+//   return fetchUtils.fetchJson(url, options);
+// }
 
-//const restClient = simpleRestClient('http://127.0.0.1:8080/admapi', httpClient);
-const restClient = simpleRestClient('/admapi', httpClient);
+//const restClient = jsonServerRestClient('http://127.0.0.1:8080/admapi', httpClient);
+const restClient = jsonServerRestClient('/admapi');
 
 const App = () => (
   <Admin title="EasyLogin" restClient={restClient}>
